4a14229059ccebb7321beb94ca8798810d61506e
Min-Jae-Bae <qoalswo123@icloud.com>
2022-04-29 14:10:49 +0900

    class Review

--- src/main/kotlin/PremierLeague.kt
+++ src/main/kotlin/PremierLeague.kt
@@ -1,12 +1,13 @@
 fun main(): Unit {
-    val liverpool = Liverpool(70)
-    val manchesterCity = ManchesterCity(90)
-    val manchesterUnited = ManchesterUnited(50)
+    val liverpool = Liverpool(70, "You get 3 points!")
+    val manchesterCity = ManchesterCity(90,"You get 3 points!")
+    val manchesterUnited = ManchesterUnited(50, "You get 3 points!")
 
     with(liverpool) {
         println("Team Name: ${liverpool.teamName}")
         println("Ranking: ${liverpool.ranking}")
         println("Victory Points: ${liverpool.victoryPoints}")
+        println("Congratulation: ${victory()}")
         println("Have a low rank? ${haveLowRank()}")
     }
 
@@ -14,6 +15,7 @@ fun main(): Unit {
         println("Team Name: ${manchesterCity.teamName}")
         println("Ranking: ${manchesterCity.ranking}")
         println("Victory Points: ${manchesterCity.victoryPoints}")
+        println("Congratulation: ${victory()}")
         println("Have a low rank? ${haveLowRank()}")
     }
 
@@ -21,6 +23,7 @@ fun main(): Unit {
         println("Team Name: ${manchesterUnited.teamName}")
         println("Ranking: ${manchesterUnited.ranking}")
         println("Victory Points: ${manchesterUnited.victoryPoints}")
+        println("Congratulation: ${victory()}")
         println("Have a low rank? ${haveLowRank()}")
     }
 }
@@ -34,33 +37,47 @@ abstract class PremierLeague(private var otherTeamVictoryPoints: Int) {
     abstract val ranking: Int
     abstract val victoryPoints: Int
 
+
     /*
     * 다른 팀의 승점이 현재 팀의 승점을 비교하는 함수 */
     fun haveLowRank(): Boolean {
         return otherTeamVictoryPoints < victoryPoints
 
     }
+    abstract fun victory() : String
 }
 
 /*
 * PremierLeague (매개변수) 슈퍼클래스를 상속받는 다른팀 승점 매개변수를 받는 Team 클래스를 만들고
 * 각각 변수를 재정의 한다*/
-class Liverpool(otherTeamVictoryPoints: Int) : PremierLeague(otherTeamVictoryPoints) {
+class Liverpool(otherTeamVictoryPoints: Int,
+                val congratulation: String) : PremierLeague(otherTeamVictoryPoints) {
     override val teamName = "Liverpool"
     override val ranking = 2
     override val victoryPoints = 79
+    override fun victory(): String {
+        return congratulation
+    }
 }
 
-open class ManchesterCity(otherTeamVictoryPoints: Int) : PremierLeague(otherTeamVictoryPoints) {
+open class ManchesterCity(otherTeamVictoryPoints: Int,
+                          val congratulation: String) : PremierLeague(otherTeamVictoryPoints) {
     override val teamName = "ManchesterCity"
     override val ranking = 1
     override val victoryPoints = 80
+
+    override fun victory(): String {
+        return congratulation + congratulation
+    }
 }
 
-class ManchesterUnited(
-    otherTeamVictoryPoints: Int) : ManchesterCity(otherTeamVictoryPoints) {
+class ManchesterUnited(otherTeamVictoryPoints: Int, congratulation: String) : ManchesterCity(otherTeamVictoryPoints, congratulation) {
     override val teamName = "ManchesterUnited"
     override val ranking = 6
     override val victoryPoints = 54
 
+    override fun victory(): String {
+        return super.victory()
+    }
 }
+
